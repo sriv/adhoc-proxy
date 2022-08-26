d44af3e1f225de415d102772ce188ef707796ef2
Srikanth <srikanth.ddit@gmail.com>
2022-08-26 14:12:09 +0530

    return stringified data

--- functions/route_20.js
+++ functions/route_20.js
@@ -4,7 +4,7 @@ const API_ENDPOINT = "http://15.207.210.8/mservice/GPSMobile/getLiveDatasWeb"
 export async function handler(event, context) {
   try {
     const {data} = await axios.post(API_ENDPOINT, { p_imeino: 869523057984560 });
-    return { statusCode: 200, body: data };
+    return { statusCode: 200, body: JSON.stringify(data) };
   } catch (error) {
     return { statusCode: 422, body: String(error) }
   }
