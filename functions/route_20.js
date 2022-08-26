5c1d25ef7c2922d9993ca9951ff0a384a40fbdae
Srikanth <srikanth.ddit@gmail.com>
2022-08-26 14:09:46 +0530

    try to use axios

--- functions/route_20.js
+++ functions/route_20.js
@@ -1,12 +1,10 @@
-import got from "got";
+import axios from 'axios';
 const API_ENDPOINT = "http://15.207.210.8/mservice/GPSMobile/getLiveDatasWeb"
 
 export async function handler(event, context) {
   try {
-    const {res} = await got.post(API_ENDPOINT, {
-      json: { p_imeino: 869523057984560 }
-    });
-    return { statusCode: 200, body: res };
+    const {data} = await axios.post(API_ENDPOINT, { p_imeino: 869523057984560 });
+    return { statusCode: 200, body: data };
   } catch (error) {
     return { statusCode: 422, body: String(error) }
   }
