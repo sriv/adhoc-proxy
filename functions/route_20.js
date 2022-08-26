1ae50a60729bd38264a92ea036f82a8d2faefac2
Srikanth <srikanth.ddit@gmail.com>
2022-08-26 12:31:28 +0530

    convert to es6 async/await

--- functions/route_20.js
+++ functions/route_20.js
@@ -2,14 +2,14 @@ import fetch from "node-fetch";
 const API_ENDPOINT = "http://15.207.210.8/mservice/GPSMobile/getLiveDatasWeb"
 
 export async function handler(event, context) {
-  return fetch(API_ENDPOINT, {
+  try {
+    const res = await fetch(API_ENDPOINT, {
       headers: { Accept: "application/json", "Content-Type": "application/json" } ,
       method: "POST",
       body: JSON.stringify({ p_imeino: 869523057984560 })
-  }).then((response) => response.json())
-    .then((data) => ({
-      statusCode: 200,
-      body: data,
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+    });
+    return { statusCode: 200, body: res };
+  } catch (error) {
+    return { statusCode: 422, body: String(error) }
+  }
 }
