94e2f768d7d4bf34f5346b2149d454b4f29b4772
Srikanth <srikanth.ddit@gmail.com>
2022-08-26 12:37:22 +0530

    use got instead of node-fetch

--- functions/route_20.js
+++ functions/route_20.js
@@ -1,12 +1,10 @@
-import fetch from "node-fetch";
+import got from "got";
 const API_ENDPOINT = "http://15.207.210.8/mservice/GPSMobile/getLiveDatasWeb"
 
 export async function handler(event, context) {
   try {
-    const res = await fetch(API_ENDPOINT, {
-      headers: { Accept: "application/json", "Content-Type": "application/json" } ,
-      method: "POST",
-      body: JSON.stringify({ p_imeino: 869523057984560 })
+    const {res} = await got.post(API_ENDPOINT, {
+      json: { p_imeino: 869523057984560 }
     });
     return { statusCode: 200, body: res };
   } catch (error) {
