aed8b1fbd8daf217fce6abfd4921fc66df921d2a
Srikanth <srikanth.ddit@gmail.com>
2022-08-27 08:10:58 +0530

    api returns GeoJSON

--- functions/route_20.js
+++ functions/route_20.js
@@ -4,7 +4,19 @@ const API_ENDPOINT = "http://15.207.210.8/mservice/GPSMobile/getLiveDatasWeb"
 export async function handler(event, context) {
   try {
     const {data} = await axios.post(API_ENDPOINT, { p_imeino: 869523057984560 });
-    return { statusCode: 200, body: JSON.stringify(data) };
+    const ping = data.ResponseData[0];
+    var resp = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [ping.lat, ping.lng]
+      },
+      "properties": {
+        "statusdatetime": ping.statusdatetime,
+        "speed": ping.speed
+      }
+    }
+    return { statusCode: 200, body: JSON.stringify(resp) };
   } catch (error) {
     return { statusCode: 422, body: String(error) }
   }
