b469205ea9a2e45d2d69d27f507a7f90350e8512
Srikanth <srikanth.ddit@gmail.com>
2022-09-02 10:50:56 +0530

    add icon + label

--- src/index.js
+++ src/index.js
@@ -22,7 +22,23 @@ var realtime = L.realtime( {
     type: "json"
   },
   {
-    interval: 3000
+    interval: 3000,
+    pointToLayer: function (feature, latlng) {
+        var marker =  L.marker(latlng, {
+            'icon': L.icon({
+                iconUrl: './src/bus.png',
+                iconSize:     [38, 95], // size of the icon
+                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            })
+        });
+        marker.bindTooltip("Route 20", 
+        {
+            permanent: true, 
+            direction: 'right'
+        });
+        return marker;
+    }
   }
 ).addTo(map);
 
