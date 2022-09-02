2151e11ee89aee857cc6e11ff91709c32b125e63
Srikanth <srikanth.ddit@gmail.com>
2022-09-02 10:54:55 +0530

    icon size

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ var realtime = L.realtime( {
         var marker =  L.marker(latlng, {
             'icon': L.icon({
                 iconUrl: './src/bus.png',
-                iconSize:     [38, 95], // size of the icon
-                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                iconSize:     [24, 24], // size of the icon
+                iconAnchor:   [22, 24], // point of the icon which will correspond to marker's location
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             })
         });
